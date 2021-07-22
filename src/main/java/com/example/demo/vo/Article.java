976edc87960b1ed46ae2b538af569a0e3b693904
hun1824 <hun1824@hanmail.net>
2021-07-23 00:36:56 +0900

    aticleVo, usrArticleController 추가

---
+++ src/main/java/com/example/demo/vo/Article.java
@@ -0,0 +1,15 @@
+package com.example.demo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+	private int id;
+	private String title;
+	private String body;
+}
